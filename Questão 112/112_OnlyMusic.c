b4f05663e001bba589de0b6c89eda139cf3f83bf
Leo <leonardo.tv@aluno.ifsc.edu.br>
2024-04-02 08:39:17 -0300

    Review 02/04/24

--- Questão 112/112_OnlyMusic.c
+++ Questão 112/112_OnlyMusic.c
@@ -3,37 +3,35 @@
 #include <time.h>
 #define rep 100000
 
+double calcula_chance(double a, double b){
+    double count;
+    for (int i = 0; i < rep; ++i) {
+        int x = rand()%100;
+        int y = rand()%100;
+        //1 de 2 programas de 1 Hora são de musica-> 0.5 de chance de musica
+        if(x<a){
+            //2 de 6 programas de 1/2 hora são de musica -> 2/6 de chance
+            if(y<b){
+                count++;
+            }
+        }
+    }
+}
+
 int main(){
 
     srand(time(NULL));
 
+    double Musica1H = 50;
+    double MusicaHalfH = 33.33;
+
     double OnlyMusicA = 0;
     //1Hour seguido halfHour
-    for (int i = 0; i < rep; ++i) {
-        int x = rand()%2;
-        int y = rand()%6;
-        //1 de 2 programas de 1 Hora são de musica-> 0.5 de chance de musica
-        if(x<1){
-            //2 de 6 programas de 1/2 hora são de musica -> 2/6 de chance
-            if(y<2){
-                OnlyMusicA++;
-            }
-        }
-    }
+    OnlyMusicA = calcula_chance(Musica1H, MusicaHalfH);
 
     double OnlyMusicB;
     //halfHour seguido de 1Hour
-    for (int i = 0; i < rep; ++i) {
-        int x = rand()%6;
-        int y = rand()%2;
-        //1 de 2 programas de 1 Hora são de musica-> 0.5 de chance de musica
-        if(x<2){
-            //2 de 6 programas de 1/2 hora são de musica -> 2/6 de chance
-            if(y<1){
-                OnlyMusicB++;
-            }
-        }
-    }
+    OnlyMusicB = calcula_chance(MusicaHalfH, Musica1H);
 
     printf("Chance do programa ser apenas de musica: %.4f\n", (OnlyMusicA/rep)*(OnlyMusicB/rep));
 
--- Questão 43/Questão_43.c
+++ Questão 43/Questão_43.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#define Rep 1000000
+#define Rep 100000
 
 int main(){
 
